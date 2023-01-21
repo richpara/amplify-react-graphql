26e63984738073bedeaea4513ec4f7a5c2c4a4da
Richard Paradies <rparadies@gmail.com>
2023-01-20 17:03:23 -0800

    Test for AWS Amplify

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and AWS
+          Learn React, Nodejs, AWS, Python
         </a>
       </header>
     </div>
